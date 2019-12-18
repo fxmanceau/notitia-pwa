1f6dfbebe35092e38aee36a907a8ad6218235375
fxmanceau <francois-xavier@MacBook-Pro-de-Francois-Xavier.local>
2019-12-18 11:30:48 +0100

    :wrench: Disable eslint

---
+++ src/views/Map/Map.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="global-map"></div>
+</template>
+
+<script>
+export default {
+  name: "Map",
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {}
+};
+</script>
+
+<style lang="scss">
+@import "Map.scss";
+</style>
