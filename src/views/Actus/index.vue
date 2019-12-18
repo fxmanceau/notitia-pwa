e421500bdecf4a7989a05bd54e816895c3f6f475
Loris Marino <loris.mro@gmail.com>
2019-12-18 15:59:32 +0100

    :construction:

--- src/views/Actus/index.vue
+++ src/views/Actus/index.vue
@@ -1,7 +1,17 @@
 <template>
+<div class="container">
   <div class="actus">
-   Actuis
+   <div class="actus__actu">
+     <img src="/img/temp/actu.jpg" alt="actu">
+     <div class="actus__content">
+      <div class="actus__tag">Intempéries</div>
+      <h1>
+        Inondations : quatre départements en vigilance orange, une troisième victime.
+      </h1>
+     </div>
+   </div>
   </div>
+</div>
 </template>
 
 <script>
