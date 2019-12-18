52017671a23013f3c51a8d9d68eec511b3a795ed
fxmanceau <francois-xavier@MacBook-Pro-de-Francois-Xavier.local>
2019-12-18 11:04:27 +0100

    :construction: Work in progress Card & Tapbar

---
+++ src/components/Map/index.vue
@@ -0,0 +1,86 @@
+<template>
+    <div id="map"></div>
+</template>
+
+<script>
+import data from '../../data.json'
+import { map, tileLayer, marker, icon } from 'leaflet'
+
+export default {
+    name: 'Map',
+    data() {
+        return {
+            map: undefined,
+        }
+    },
+    mounted() {
+        this.map = map('map').setView(
+            data.dashboard.map.townLatLng,
+            data.dashboard.map.initialZoom,
+        )
+
+        tileLayer(
+            'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+            {
+                attribution:
+                    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+                subdomains: 'abcd',
+                maxZoom: 25,
+            },
+        ).addTo(this.map)
+
+        this.map.on('click', e => {
+            console.log('LatLng: ', e.latlng)
+        })
+
+        data.dashboard.map.incidentLatLngs.forEach(incident => {
+            this.addMarker(incident.type, ...incident.latLng)
+            const [lang, _] = this.parseRoute()
+        })
+    },
+    methods: {
+        addMarker(type, lat, lng) {
+            const iconEl = this.makeIcon(type)
+
+            const markerEl = marker([lat, lng], { icon: iconEl })
+            markerEl.addTo(this.map)
+            markerEl.on('click', e => {
+                console.log(e)
+            })
+        },
+        makeIcon(type) {
+            let iconName = ''
+            switch (type) {
+                case 'flooding':
+                    iconName = 'flooding'
+                    break
+                case 'shelter':
+                    iconName = 'home'
+                    break
+                case 'industry':
+                    iconName = 'industry'
+                    break
+                case 'electricity':
+                    iconName = 'electricity'
+                    break
+                case 'worksite':
+                    iconName = 'worksite'
+                    break
+                default:
+                    throw new Error(
+                        "Le `type` pour un marker sur la carte n'est pas valide",
+                    )
+                    break
+            }
+
+            return icon({
+                iconUrl: require(`../../assets/${iconName}-marker.png`),
+                iconSize: [38.25, 46.125],
+                iconAnchor: [19.125, 46.125],
+            })
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped src="./style.scss"></style>
---
+++ src/components/Nav-Bar/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <nav class="nav-bar-container">
+        <div class="nav-bar-list">
+            <router-link :to="`/${lang}/alert`" class="nav-bar-item">
+                <img
+                    class="item-image"
+                    :src="`/img/navbar/pin${route == 'alert' ? '-active' : ''}.svg`"
+                    alt="Ma commune"
+                />
+                <span class="item-title">Ma commune</span>
+            </router-link>
+            <router-link :to="`/${lang}/danger`" class="nav-bar-item">
+                <img
+                    class="item-image"
+                    :src="`/img/navbar/home${route == 'danger' ? '-active' : ''}.svg`"
+                    alt="Informations"
+                />
+                <span class="item-title">Informations</span>
+            </router-link>
+            <router-link :to="`/${lang}/dashboard`" class="nav-bar-item">
+                <img
+                    class="item-image"
+                    :src="`/img/navbar/alert${route == 'dashboard' ? '-active' : ''}.svg`"
+                    alt="Urgence"
+                />
+                <span class="item-title">Urgence</span>
+            </router-link>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    props: ['route', 'lang'],
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'style.scss';
+</style>
